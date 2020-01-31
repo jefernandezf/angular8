6daac95e0bd29499927754313d4739c445b88edb
junior <junior670@hotmail.com>
2020-01-31 00:11:58 -0500

    proyecto enviando al repo local

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DadoComponent } from './dado/dado.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DadoComponent
   ],
   imports: [
     BrowserModule,
